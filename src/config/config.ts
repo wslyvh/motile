8056415b0bda12df0e7840f24f256ae9f69a28f8
wslyvh <wslyvh@users.noreply.github.com>
2019-10-31 18:09:09 +0100

    bitmex env

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,15 @@ const AppConfig = {
   PORT: process.env.PORT ? parseInt(process.env.PORT, 10) : 5000,
 
   LOG_DIR: process.env.LOG_DIR || "logs",
-  LOG_LEVEL: process.env.NODE_ENV === "production" ? "error" : "debug"
+  LOG_LEVEL: process.env.NODE_ENV === "production" ? "error" : "debug",
+
+  BITMEX_ID: process.env.BITMEX_ID,
+  BITMEX_SECRET: process.env.BITMEX_SECRET
 };
+
+if (!AppConfig.BITMEX_ID && !AppConfig.BITMEX_SECRET) {
+  // tslint:disable-next-line: no-console
+  console.log("BITMEX_ID or BITMEX_SECRET not correctly set als env variables.");
+}
 
 export = AppConfig;
