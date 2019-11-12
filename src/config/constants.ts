340d4af9f3df228ac6423615096ce5974d02a53a
wslyvh <wslyvh@users.noreply.github.com>
2019-11-12 14:37:19 +0100

    constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,11 +1,11 @@
 export class Constants {
   public static readonly EXECUTE_MODE = true;
 
-  public static readonly DEFAULT_RISK_LEVEL = 0.015;
-  public static readonly DEFAULT_SPREAD = 0.001;
-  public static readonly DEFAULT_PERCENTAGE_AT_RISK = 20;
-  public static readonly DEFAULT_RANGE = 0.002;
+  public static readonly DEFAULT_PERCENTAGE_AT_RISK = 20; // Percentage of total balance / at Stake
+  public static readonly DEFAULT_RISK_LEVEL = 0.015; // Percentage of total balance / per orderSize
+  public static readonly DEFAULT_SPREAD = 0.001; // Percentage from current price
+  public static readonly DEFAULT_RANGE = 0.002; // Percentage from current price
 
   public static readonly DEFAULT_ORDER_AMOUNT = 5;
-  public static readonly DEFAULT_OPEN_ORDERS = 10;
+  public static readonly DEFAULT_OPEN_ORDERS = 15;
 }
