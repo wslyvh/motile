a9c15a304f0181abe4180037cab565c24256b35e
wslyvh <wslyvh@users.noreply.github.com>
2020-01-05 13:02:05 +0100

    open orders range

---
+++ src/conditions/OpenOrdersRange.ts
@@ -0,0 +1,32 @@
+import { IBroker } from "../broker/IBroker";
+import logger from "../utils/Logger";
+import { ICondition } from "./ICondition";
+
+export class OpenOrdersRange implements ICondition {
+  private RANGE: number;
+  private broker: IBroker;
+
+  constructor(value: number, broker: IBroker) {
+    this.RANGE = value;
+    this.broker = broker;
+  }
+
+  public async ShouldExecute(): Promise<boolean> {
+    const orders = await this.broker.getOpenOrders();
+    const price = await this.broker.price();
+
+    orders.forEach(order => {
+      const position = order.Price;
+      const spread = price * this.RANGE;
+      const upperBound = price + spread;
+      const lowerBound = price - spread;
+
+      if (position && position < upperBound && position > lowerBound) {
+        logger.warn("Current Price within open order range: " + position + ". Stop execution.");
+        return false;
+      }
+    });
+
+    return true;
+  }
+}
