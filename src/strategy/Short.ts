f357c3d65e0787536b8f0e86f4c903b2b5a24299
wslyvh <wslyvh@users.noreply.github.com>
2020-01-05 13:11:26 +0100

    order ranges

--- src/strategy/Short.ts
+++ src/strategy/Short.ts
@@ -2,6 +2,7 @@ import { SellOrder } from "../actions/SellOrder";
 import { UpdatePositionClose } from "../actions/UpdatePositionClose";
 import { IBroker } from "../broker/IBroker";
 import { PercentagePositionSize, PositionRange } from "../conditions";
+import { OpenOrdersRange } from "../conditions/OpenOrdersRange";
 import AppConfig from "../config/config";
 import { Strategy } from "./Strategy";
 
@@ -15,6 +16,7 @@ export class Short extends Strategy {
 
     this.Conditions.push(new PercentagePositionSize(AppConfig.DEFAULT_PERCENTAGE_AT_RISK, this.broker));
     this.Conditions.push(new PositionRange(AppConfig.DEFAULT_RANGE, this.broker));
+    this.Conditions.push(new OpenOrdersRange(AppConfig.DEFAULT_RANGE, this.broker));
 
     this.Actions.push(new SellOrder(this.broker));
     this.Actions.push(new UpdatePositionClose(this.broker));
