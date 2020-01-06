8e5f9eeba2e9564049c35a973db069c50d04ca4f
wslyvh <wslyvh@users.noreply.github.com>
2020-01-06 21:50:18 +0100

    cancel close order

--- src/actions/UpdatePositionClose.ts
+++ src/actions/UpdatePositionClose.ts
@@ -1,4 +1,4 @@
-import { IBroker } from "../broker/IBroker";
+import { IBroker, IPosition } from "../broker/IBroker";
 import AppConfig from "../config/config";
 import logger from "../utils/Logger";
 import { IAction } from "./IAction";
@@ -18,29 +18,56 @@ export class UpdatePositionClose implements IAction {
       return false;
     }
 
-    // Cancel Open orders
-    await this.broker.cancelOpenOrders();
-
     let price = Math.abs(position.Entry);
     const orderSize = Math.abs(position.Size);
     const spread = position.Entry * AppConfig.DEFAULT_SPREAD;
     const currentPrice = await this.broker.price();
 
     if (position.Size < 0) {
-      logger.info("Update Position close: " + orderSize + ". SELL @ " + (price + spread));
-      if (currentPrice > price + spread) {
-        price = currentPrice;
+      const result = await this.cancelClosingOrder("sell", position);
+      if (result) {
+        logger.info("Update Position close: " + orderSize + ". SELL @ " + (price + spread));
+        if (currentPrice > price + spread) {
+          price = currentPrice;
+        }
+        await this.broker.createSellOrder(orderSize, price + spread);
+      } else {
+        logger.info("Order.Quantity and Position.Size are equal. Stop execution");
+        return false;
       }
-      await this.broker.createSellOrder(orderSize, price + spread);
     }
+
     if (position.Size > 0) {
-      logger.info("Update Position close: " + orderSize + ". BUY @ " + (price - spread));
-      if (currentPrice < price - spread) {
-        price = currentPrice;
+      const result = await this.cancelClosingOrder("buy", position);
+      if (result) {
+        logger.info("Update Position close: " + orderSize + ". BUY @ " + (price - spread));
+        if (currentPrice < price - spread) {
+          price = currentPrice;
+        }
+        await this.broker.createBuyOrder(orderSize, price - spread);
+      } else {
+        logger.info("Order.Quantity and Position.Size are equal. Stop execution");
+        return false;
       }
-      await this.broker.createBuyOrder(orderSize, price - spread);
     }
 
     return true;
+  }
+
+  private async cancelClosingOrder(side: "sell" | "buy", position: IPosition) {
+    const orders = await this.broker.getOpenOrders(side);
+    if (orders && orders.length > 0) {
+      const closeOrder = orders[0];
+      if (closeOrder.Quantity === position.Size || -closeOrder.Quantity === position.Size) {
+        console.log("NO NEED TO UPDATE");
+        return false;
+      }
+
+      logger.info("Cancelling existing closing " + side + " order");
+      await this.broker.cancelOrder(closeOrder.Id);
+      return true;
+    }
+
+    return false;
   }
 }
